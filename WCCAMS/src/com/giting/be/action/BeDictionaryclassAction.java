56c253360f25d05aeb11c3f2593d568ae01557af
fuchenggang <838493503@qq.com>
2014-09-02 19:25:48 +0800

    action

---
+++ WCCAMS/src/com/giting/be/action/BeDictionaryclassAction.java
@@ -0,0 +1,37 @@
+package com.giting.be.action;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+
+import com.giting.be.service.IBeDictionaryclassService;
+import com.opensymphony.xwork2.ActionSupport;
+/**   
+*    
+* 项目名称：WCCAMS   
+* 类名称：BeDictionaryclassAction   
+* 类描述：   字典类型表对应的数据交换层
+* 创建人：fuchenggang@foxmail.com   
+* 创建时间：2014-9-2 上午12:12:49   
+* 修改人：  
+* 修改时间：  
+* 修改备注：   
+* @version    
+*    
+*/ 
+@Controller("beDictionaryclassAction")
+public class BeDictionaryclassAction extends ActionSupport{
+
+	/** 
+	* @Fields serialVersionUID : 序列号
+	*/ 
+	private static final long serialVersionUID = 1L;
+	
+	@Resource
+	private IBeDictionaryclassService beDictionaryclassService;
+
+	public void setBeDictionaryclassService(IBeDictionaryclassService beDictionaryclassService) {
+		this.beDictionaryclassService = beDictionaryclassService;
+	}
+	
+}
---
+++ WCCAMS/src/com/giting/be/action/BeDictionarydataAction.java
@@ -0,0 +1,42 @@
+package com.giting.be.action;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+
+import com.giting.be.service.IBeDictionarydataService;
+import com.opensymphony.xwork2.ActionSupport;
+
+/**   
+*    
+* 项目名称：WCCAMS   
+* 类名称：BeDictionarydataAction   
+* 类描述： 字典数据表对应的数据交换层
+* 创建人：fuchenggang@foxmail.com   
+* 创建时间：2014-9-1 下午11:59:08   
+* 修改人：  
+* 修改时间：  
+* 修改备注：   
+* @version    
+*    
+*/ 
+@Controller("beDictionarydataAction")
+public class BeDictionarydataAction extends ActionSupport{
+
+	/** 
+	* @Fields serialVersionUID : 序列号 
+	*/ 
+	private static final long serialVersionUID = 1L;
+	
+	/** 
+	* @Fields beDictionarydataService : TODO(用一句话描述这个变量表示什么) 
+	*/ 
+	@Resource
+	private IBeDictionarydataService beDictionarydataService;
+
+	public void setBeDictionarydataService(IBeDictionarydataService beDictionarydataService) {
+		this.beDictionarydataService = beDictionarydataService;
+	}
+
+	
+}
