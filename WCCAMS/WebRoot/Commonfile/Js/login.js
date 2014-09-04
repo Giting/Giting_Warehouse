3f96d13959d16dc0055da6805c2c7bfb6f4fe526
fuchenggang <838493503@qq.com>
2014-09-04 14:13:26 +0800

    登录界面

---
+++ WCCAMS/WebRoot/Commonfile/Js/login.js
@@ -0,0 +1,35 @@
+/** 
+* @fileOverview 系统登录模块操作JS文件  
+* @author <font color="red" size="5">fuchenggang</font>
+* @version 0.1 
+*/ 
+$(function(){
+	
+	/** 
+	* @author <font color="red" size="5">fuchenggang</font> 
+	* @constructor LoginObject 
+	* @description 一个登录对象的全部URL请求资源 
+	* @param {Object} param 参数对象 
+	*/ 
+	var GlobalVariable ={
+			loginUrl :""
+	}
+	
+	/** 
+	* @author <font color="red" size="5">fuchenggang</font> 
+	* @constructor LoginObject 
+	* @description 一个登录对象 
+	* @param {Object} param 参数对象 
+	*/ 
+	var LoginObject={
+		username ="",
+		password = "",
+		commonuser = "",
+		loginFunctin : function(param) {
+			 $('#btnLogin').click(
+					 
+			 );
+		},
+	} 
+	
+}
