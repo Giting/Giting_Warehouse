d9176e273a48e6e12e6d8700bcc0d0ccfa2eba40
fuchenggang <838493503@qq.com>
2014-09-05 14:01:31 +0800

    状态码

--- WCCAMS/src/com/giting/util/CheckCode.java
+++ WCCAMS/src/com/giting/util/CheckCode.java
@@ -39,10 +39,43 @@ public class CheckCode {
 	 * @Fields deleteMap : 删除失败信息
 	 */
 	private static Map<String, Object> deleteMapError;
+	/**
+	 * @Fields deleteMap : 登录成功信息
+	 */
+	private static Map<String, Object> loginMapSuccess;
+	/**
+	 * @Fields deleteMap : 登录失败信息
+	 */
+	private static Map<String, Object> loginMapError;
+	
+	/** 
+	* @Fields userError : 用户名错误 
+	*/ 
+	public final static int USERERROR = 2;
+	/** 
+	* @Fields userErrorMsg : 用户名错误信息
+	*/ 
+	public final static String USERERRORMSG = "用户名错误!";
+	/** 
+	* @Fields passWordError : 密码错误
+	*/ 
+	public final static int PASSWORDERROR = 3;
+	/** 
+	* @Fields passWordErrorMsg : 密码错误信息 
+	*/ 
+	public final static String PASSWORDERRORMSG = "密码错误!";
+	/** 
+	* @Fields commonuserNameError : 社区名称错误 
+	*/ 
+	public final static  int COMMONUSERNAMEERROR =4;
+	/** 
+	* @Fields commonuserNameMsg : 社区名称错误信息
+	*/ 
+	public final static String COMMONUSERNAMEEERRORMSG = "登录社区错误!";
 
 	/**
 	 * @Title: getAddMapSuccess
-	 * @Description: TODO(这里用一句话描述这个方法的作用)
+	 * @Description: 添加成功信息
 	 * @return Map<String,Object> 返回类型
 	 * @throws
 	 */
@@ -117,5 +150,68 @@ public class CheckCode {
 		deleteMapError.put("msg", "删除失败！");
 		return deleteMapError;
 	}
+
+	/** 
+	 * @Title: getLoginMapSuccess 
+	 * @Description: 登录成功信息 
+	 * @param 设定文件 
+	 * @return Map<String,Object> 返回类型 
+	 * @throws 
+	 */ 
+	public static Map<String, Object> getLoginMapSuccess() {
+		loginMapSuccess = new HashMap<String, Object>();
+		loginMapSuccess.put("result", false);
+		loginMapSuccess.put("msg", "登录成功！");
+		return loginMapSuccess;
+	}
+
+	public static Map<String, Object> getLoginMapError(int stateInt,String stateMsg) {
+		loginMapError = new HashMap<String, Object>();
+		loginMapError.put("result", stateInt);
+		loginMapError.put("msg", stateMsg);
+		return loginMapError;
+	}
+	
+	/** 
+	 * @Title: getLoginMapUserError 
+	 * @Description: 登录用户名错误
+	 * @param stateInt
+	 * @param stateMsg
+	 * @param 设定文件 
+	 * @return Map<String,Object> 返回类型 
+	 * @throws 
+	 */ 
+	public final static Map<String, Object> getLoginMapUserError(int stateInt,String stateMsg) {
+		return getLoginMapError(USERERROR,USERERRORMSG);
+	}
+	
+	/** 
+	 * @Title: getLoginMapPassWordError 
+	 * @Description: 登陆密码错误
+	 * @param stateInt
+	 * @param stateMsg
+	 * @param 设定文件 
+	 * @return Map<String,Object> 返回类型 
+	 * @throws 
+	 */ 
+	public static Map<String, Object> getLoginMapPassWordError(int stateInt,String stateMsg) {
+		return getLoginMapError(PASSWORDERROR,PASSWORDERRORMSG);
+	}
+	
+	/** 
+	 * @Title: getLoginMapCommonuserNameError 
+	 * @Description: 登录社区错误 
+	 * @param stateInt
+	 * @param stateMsg
+	 * @param 设定文件 
+	 * @return Map<String,Object> 返回类型 
+	 * @throws 
+	 */ 
+	public static Map<String, Object> getLoginMapCommonuserNameError(int stateInt,String stateMsg) {
+		return getLoginMapError(COMMONUSERNAMEERROR,COMMONUSERNAMEEERRORMSG);
+	}
+	
+	
+
 
 }
